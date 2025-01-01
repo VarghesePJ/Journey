5e5ac93ef30f520ea6710ce3050ebfb743230f6d
VarghesePJ <vargheseputhuvajoseph@gmail.com>
2025-01-01 14:37:03 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,10 @@ const Hero = () => {
         <>
             <section class="relative py-10 overflow-hidden bg-black sm:py-16 lg:py-24 xl:py-32">
                 <div class="absolute inset-0">
-                    <video class="object-cover w-full h-full md:object-rigth md:origin-top-rigth aspect-video" src={videoFile} loop autoPlay muted ></video>
+                    <video class="object-cover w-full h-full md:object-right md:origin-top-right aspect-video" src={videoFile} loop autoPlay muted></video>
                 </div>
-
                 <div class="absolute inset-0 hidden bg-gradient-to-r md:block from-black to-transparent"></div>
-
                 <div class="absolute inset-0 block bg-black/60 md:hidden"></div>
-
                 <div class="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                     <div class="text-center md:w-2/3 lg:w-1/2 xl:w-1/2 md:text-left">
                         <h2 class="text-3xl font-bold leading-tight sm:text-4xl lg:text-5xl bg-gradient-to-r from-amber-200 to-yellow-500 inline-block text-transparent bg-clip-text">Discover the Magic of the UAE</h2>
